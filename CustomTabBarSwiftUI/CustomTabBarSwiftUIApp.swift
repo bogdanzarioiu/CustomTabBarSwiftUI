221b284cb568da19b2d8bebc724e9cfa706c9910
Bogdan Zarioiu <bogdanzarioiu@gmail.com>
2021-02-07 14:02:15 +0100

    Initial Commit

---
+++ CustomTabBarSwiftUI/CustomTabBarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarSwiftUIApp.swift
+//  CustomTabBarSwiftUI
+//
+//  Created by Bogdan on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
